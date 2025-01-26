41674867c8d502ebb0644080787f4cf42ea793ec
chloe <chloe.leyssens@telecom-paris.fr>
2025-01-26 17:40:48 +0100

    create smoothPath function that will generate all new smooth path

--- Assets/Scripts/ImprovePath.cs
+++ Assets/Scripts/ImprovePath.cs
@@ -7,6 +7,43 @@ using Scripts.Map;
 
 public class ImprovePath
 {
+    List<Vector3> SmoothPath(List<Vector3> path) // Create longer path smoother
+    {
+        List<Vector3> smoothedPath = new List<Vector3>();
+        int n=path.Count;
+        for (int i = 0; i < n- 1; i++)
+        {
+            Vector3 p0;
+            if (i==0)
+            {
+                p0 = path[i];
+            }
+            else
+            {
+                p0 = path[i - 1];
+            }
+            Vector3 p1 = path[i];
+            Vector3 p2 = path[i + 1];
+            Vector3 p3;
+            if (i==n-2)
+            {
+                p3 = path[i+1];
+            }
+            else
+            {
+                p3 = path[i+2];
+            }
+
+            for (float t = 0; t < 1f; t += 0.2f) // Decreasing step will increase number of points
+            {
+                Vector3 newPoint = CatmullRom(p0, p1, p2, p3, t);
+                smoothedPath.Add(newPoint);
+            }
+        }
+        smoothedPath.Add(path[n - 1]); // Add last point
+        return smoothedPath;
+    }
+
     Vector3 Catmull_Rom(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) // interpolate new point between p0 and p1
     {
         float t2 = t * t;
