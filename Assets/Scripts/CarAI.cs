9e3374ade41a598913e6938bf89de108decb0d14
Alexandra Papadopoulos <apapado@kth.se>
2025-02-05 16:04:03 +0100

    Final version

--- Assets/Scripts/CarAI.cs
+++ Assets/Scripts/CarAI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 using Scripts.Vehicle;
 using Scripts.Map;
 using Imported.StandardAssets.Vehicles.Car.Scripts;
@@ -25,34 +26,36 @@ public class CarAI : MonoBehaviour
     public Vector3 old_target_pos;
     public Vector3 desired_velocity;
 
-    public float K_p = 10f;
-    public float K_d = 5f;
+    public float k_p = 1f;
+    public float k_d = 2f;
 
     public Rigidbody my_rigidbody;
 
 
     private int currentPathIndex = 1;
     private List<Vector3> path;
+    
+    private bool isStuck = false;
+    private float reverseDuration = 0;
+    private bool checkNewPoint = true;
+    
 
 
     private void Start()
     {
-        this.enabled = true;
         carCollider = gameObject.transform.Find("Colliders/ColliderBottom").gameObject.GetComponent<BoxCollider>();
         // get the car controller
         m_Car = GetComponent<CarController>();
         mapManager = FindFirstObjectByType<GameManagerA1>().mapManager;
 
         Vector3 grid_size = new Vector3(1,1,1)*1.6f; //can multiply *3 for instance
-        obstacleMap = ObstacleMap.Initialize(mapManager, new List<GameObject>(), grid_size);
+        //Vector3 margin = new Vector3(1,1,1)*1f;
+        obstacleMap = ObstacleMap.Initialize(mapManager, new List<GameObject>(), grid_size );
         my_rigidbody = GetComponent<Rigidbody>();
-
-        //k_p = 2f;
-        //k_d = 1.5f;
+        
 
         //Get starting position
         Vector3 start_pos = mapManager.GetGlobalStartPosition();
-        Debug.Log(start_pos);
         // Get goal position
         Vector3 goal_pos = mapManager.GetGlobalGoalPosition();
 
@@ -63,6 +66,7 @@ public class CarAI : MonoBehaviour
         if (first_path.Count > 0)
         {
             //this.path = improvePath.smoothPath(first_path); // doesn't work well
+            float epsilon = 0.1f;
             this.path = improvePath.smoothPath(first_path);
         }
         else
@@ -114,6 +118,7 @@ public class CarAI : MonoBehaviour
             Debug.DrawRay(transformPosition, closestObstacleInFront, Color.red);
             Debug.Log("Did Hit");
         }*/
+        
     }
 
 
@@ -128,35 +133,94 @@ public class CarAI : MonoBehaviour
         
         if (path.Count !=0 && currentPathIndex < path.Count)
         {
-            Vector3 target_position = path[currentPathIndex];
-            target_velocity = (target_position - old_target_pos) / Time.fixedDeltaTime;
-            old_target_pos = target_position;
-
-            float myK_p = 10f;
-            float myK_d = 8f;
-
-            // a PD-controller to get desired acceleration from errors in position and velocity
-            Vector3 position_error = target_position - transform.position;
-            Vector3 velocity_error = target_velocity - my_rigidbody.linearVelocity;
-            Vector3 desired_acceleration = myK_p * position_error + myK_d * velocity_error;
-
-            float steering = Vector3.Dot(desired_acceleration, transform.right);
-            float acceleration = Vector3.Dot(desired_acceleration, transform.forward);
-
-            Debug.DrawLine(target_position, target_position + target_velocity, Color.red);
-            Debug.DrawLine(transform.position, transform.position + my_rigidbody.linearVelocity, Color.blue);
-            Debug.DrawLine(transform.position, transform.position + desired_acceleration, Color.yellow);
-
-            // this is how you control the car
-            //Debug.Log("Steering:" + steering + " Acceleration:" + acceleration);
-            m_Car.Move(steering, acceleration, acceleration, 0f);
-            
-
-            if (Vector3.Distance(target_position,transform.position) <6f)
+            Debug.Log(my_rigidbody.linearVelocity.magnitude);
+            if (!isStuck)
             {
-                currentPathIndex++;
+                // Get the car's current forward direction
+                Vector3 forward = transform.forward;
+
+// Rotate the forward vector by ±30° to get left/right directions
+                Vector3 directionRight = Quaternion.Euler(0, -30, 0) * forward;
+                Vector3 directionLeft = Quaternion.Euler(0, 30, 0) * forward;
+
+                Vector3 target_position = path[currentPathIndex];
+                Debug.Log("target" + (target_position));
+                target_velocity = (target_position - old_target_pos) / Time.fixedDeltaTime;
+                old_target_pos = target_position;
+
+                // a PD-controller to get desired acceleration from errors in position and velocity
+                Vector3 position_error = target_position - transform.position;
+                Vector3 velocity_error = target_velocity - my_rigidbody.linearVelocity;
+                Vector3 desired_acceleration = k_p * position_error + k_d * velocity_error;
+
+                float steering = Vector3.Dot(desired_acceleration, transform.right);
+                float acceleration = Vector3.Dot(desired_acceleration, transform.forward);
+
+                Debug.DrawLine(target_position, target_position + target_velocity, Color.red);
+                Debug.DrawLine(transform.position, transform.position + my_rigidbody.linearVelocity, Color.blue);
+                Debug.DrawLine(transform.position, transform.position + desired_acceleration, Color.yellow);
+                
+                RaycastHit hitRight;
+                RaycastHit hitLeft;
+                RaycastHit hitStraight;
+                float maxRangeClose = 7f;
+                
+// Cast the ray in world space (no need for TransformDirection)
+                bool obsRighetClose = Physics.Raycast(transform.position, directionRight, out hitRight, maxRangeClose); 
+                bool obsLeftClose = Physics.Raycast(transform.position, directionLeft, out hitLeft, maxRangeClose);
+                bool obsStraightClose = Physics.Raycast(transform.position,
+                    transform.TransformDirection(new Vector3(0, 0, 1)), out hitStraight, maxRangeClose);
+                if (obsRighetClose)
+                {
+                    Vector3 closestObstacle = directionRight * hitRight.distance;
+                    Debug.DrawRay(transform.position, closestObstacle, Color.green);
+                    Debug.Log("Did Hit Right");
+                    steering += 10;
+                }
+
+                if (obsLeftClose)
+                {
+                    Vector3 closestObstacle = directionLeft * hitLeft.distance;
+                    Debug.DrawRay(transform.position, closestObstacle, Color.green);
+                    Debug.Log("Did Hit Left");
+                    steering -= 10;
+                }
+                
+                // this is how you control the car
+                //Debug.Log("Steering:" + steering + " Acceleration:" + acceleration);
+                m_Car.Move(steering, acceleration, acceleration, 0f);
+
+                float distToPoint = 6;
+
+                Debug.Log("dist"+Vector3.Distance(target_position, transform.position));
+                if (Vector3.Distance(target_position, transform.position) < distToPoint)
+                {
+                    checkNewPoint = true;
+                    currentPathIndex++;
+                }
+
+                if (my_rigidbody.linearVelocity.magnitude <  0.05 && (obsStraightClose || obsRighetClose|| obsLeftClose))
+                {
+                    isStuck = true;
+                }
+               
+            }
+            else
+            {
+                m_Car.Move(0f, 0f, -1f,0f);
+                reverseDuration += Time.deltaTime;
+                if (reverseDuration > 1f)
+                {
+                    isStuck = false;
+                    reverseDuration = 0f;
+                }
+                
             }
         }
+
+
+
+
     }
 
     
